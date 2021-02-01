fce1e7d5eb2994e5a99e558ec8a19c730ad13fcb
Fred Liu <hunterfredliu@gmail.com>
2021-01-31 23:02:24 -0600

    Updates

---
+++ precache-manifest.3166226a0b25601faf28d27a6b38b75c.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5381ba12610170ccf06f1a4634ee45c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ed069a96caf6af10dde8",
+    "url": "/portfolio/static/css/main.7b5e6d5d.chunk.css"
+  },
+  {
+    "revision": "d471fd6d6c473bef5520",
+    "url": "/portfolio/static/js/2.f9214dd9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.f9214dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed069a96caf6af10dde8",
+    "url": "/portfolio/static/js/main.b92d3742.chunk.js"
+  },
+  {
+    "revision": "8c78779c98e297eec2a1",
+    "url": "/portfolio/static/js/runtime-main.23e48518.js"
+  },
+  {
+    "revision": "e7ce412157b3a2e36033d015cd0bf2ad",
+    "url": "/portfolio/static/media/1.e7ce4121.png"
+  },
+  {
+    "revision": "31a43d33a87090058349160e9aff4114",
+    "url": "/portfolio/static/media/2.31a43d33.jpg"
+  },
+  {
+    "revision": "ba983b4c003b9c3b2647903e82d2171d",
+    "url": "/portfolio/static/media/3.ba983b4c.jpg"
+  },
+  {
+    "revision": "032960dcb3afb7e9ad8e60792480549e",
+    "url": "/portfolio/static/media/4.032960dc.jpg"
+  },
+  {
+    "revision": "2db4928a0dba27dfb28352e23057dd59",
+    "url": "/portfolio/static/media/5.2db4928a.jpg"
+  },
+  {
+    "revision": "6e1e386a0cc83ac2f5b2a299b0419861",
+    "url": "/portfolio/static/media/6.6e1e386a.png"
+  },
+  {
+    "revision": "3ffc08dd9834727b197b8ed0074e2ddb",
+    "url": "/portfolio/static/media/AirCompressor.3ffc08dd.jpg"
+  },
+  {
+    "revision": "3ffc08dd9834727b197b8ed0074e2ddb",
+    "url": "/portfolio/static/media/AirCompressor1.3ffc08dd.jpg"
+  },
+  {
+    "revision": "098c8a2456caed2172a90f6952df4206",
+    "url": "/portfolio/static/media/Freeze1.098c8a24.png"
+  },
+  {
+    "revision": "90ccebf63125d1a62b75cba2d4e2a62e",
+    "url": "/portfolio/static/media/FreezeFinal.90ccebf6.png"
+  },
+  {
+    "revision": "cd90d6dd11be871174d2123f3121cca2",
+    "url": "/portfolio/static/media/Gun1.cd90d6dd.png"
+  },
+  {
+    "revision": "9233ef37ae75d7e295ae511534d6334d",
+    "url": "/portfolio/static/media/Gun_Jun2020.9233ef37.png"
+  },
+  {
+    "revision": "29ba1087060eaee2687f98c1f123d41f",
+    "url": "/portfolio/static/media/Horror_House_May2019.29ba1087.jpg"
+  },
+  {
+    "revision": "29ba1087060eaee2687f98c1f123d41f",
+    "url": "/portfolio/static/media/House1.29ba1087.jpg"
+  },
+  {
+    "revision": "0adc9558c9f63914467ceaf383dd93f2",
+    "url": "/portfolio/static/media/OtherWorld1.0adc9558.jpg"
+  },
+  {
+    "revision": "0adc9558c9f63914467ceaf383dd93f2",
+    "url": "/portfolio/static/media/Other_World_Aug2018.0adc9558.jpg"
+  },
+  {
+    "revision": "f68a90b01c38b62893297a6f759eaf4c",
+    "url": "/portfolio/static/media/PuzzleBox_Banner.f68a90b0.png"
+  },
+  {
+    "revision": "669cb2dc58ad3323284aacf20226b3a6",
+    "url": "/portfolio/static/media/Rock_Aug2020.669cb2dc.png"
+  },
+  {
+    "revision": "c720fb4f16d69cc3c5dba61bc96cdc0d",
+    "url": "/portfolio/static/media/arrow-left.c720fb4f.cur"
+  },
+  {
+    "revision": "ef69e791ae57424b1901a28e2ad4d708",
+    "url": "/portfolio/static/media/arrow-right.ef69e791.cur"
+  },
+  {
+    "revision": "50f15a1c62e8f493400f0882130f40f3",
+    "url": "/portfolio/static/media/bg.50f15a1c.jpg"
+  },
+  {
+    "revision": "1813f9d645d23e27edd233139d151be4",
+    "url": "/portfolio/static/media/bg2.1813f9d6.jpg"
+  },
+  {
+    "revision": "7f7796d84a07cfc2f47cfe5a6477f147",
+    "url": "/portfolio/static/media/bg3.7f7796d8.jpg"
+  },
+  {
+    "revision": "2a127923e74349d908cd4070d3e3bc78",
+    "url": "/portfolio/static/media/bone.2a127923.jpeg"
+  },
+  {
+    "revision": "146b636e95f11d09fb3b37141751c058",
+    "url": "/portfolio/static/media/head.146b636e.jpeg"
+  },
+  {
+    "revision": "abf67243515ead003c17d2ddeefe3844",
+    "url": "/portfolio/static/media/jade-luo-highlowpoly.abf67243.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "311ef2ba24c7f5dcb2b4569752df598e",
+    "url": "/portfolio/static/media/sculpture.311ef2ba.jpeg"
+  },
+  {
+    "revision": "81052b5d76a2a96ad4e1d835ef79a298",
+    "url": "/portfolio/static/media/studio-1.81052b5d.jpg"
+  },
+  {
+    "revision": "e55751243c38b3708bfa3777b64a2cee",
+    "url": "/portfolio/static/media/studio-2.e5575124.jpg"
+  },
+  {
+    "revision": "a0bb3376dbf030f9a53dbf34ed7bc01e",
+    "url": "/portfolio/static/media/studio-3.a0bb3376.jpg"
+  },
+  {
+    "revision": "fb23554db07eaa1934d06a73f32b69d5",
+    "url": "/portfolio/static/media/studio-4.fb23554d.jpg"
+  },
+  {
+    "revision": "733c99d74daea11e9897328d6ad81970",
+    "url": "/portfolio/static/media/studio-5.733c99d7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e33ab21d0637b37ba5d68c7f1745247.js"
+  "/portfolio/precache-manifest.3166226a0b25601faf28d27a6b38b75c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
